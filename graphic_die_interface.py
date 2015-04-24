9287c14b64dbff4f6e4f542ef3fcc51943bb8813
fushinoryuu <u0556573@utah.edu>
2015-04-23 22:33:43 -0600

    Changed 1 file: graphic_die_interface.py
    Finished writing up the interface class.

--- graphic_die_interface.py
+++ graphic_die_interface.py
@@ -30,6 +30,7 @@ class GraphicDieInterface:
         self.lite_turquoise = (0, 164, 151)
         self.orange = (176, 67, 17)
         self.grey = (50, 50, 50)
+        self.yellow = (192, 150, 24)
         self.background_color = (0, 147, 134)
         self.display_surface.fill(self.background_color)
 
@@ -91,6 +92,75 @@ class GraphicDieInterface:
             if not x.HOLD:
                 x.set_random_value()
 
+    def all_buttons_inactive(self):
+        """Make all buttons in list inactive."""
+        for i in self.buttons_list:
+            i.ACTIVE = False
+
+    def start_setup(self):
+        self.all_buttons_inactive()
+        self.b1.ACTIVE = True
+        self.all_dice_inactive()
+        self.all_dice_roll()
+
+    def add_points(self):
+        self.score_tuple = self.game_rules.score_dice(self.die_object_list)
+        self.game_rules.points += self.score_tuple[1]
+
+    def display_score(self):
+        if self.score_active:
+            score_string = self.score_tuple[0] + ', ' + 'You win' + str(self.score_tuple[1]) + ' Points'
+            self.score_surface = self.game_font.render(score_string, True, self.yellow, None)
+            self.score_surface = pygame.transform.scale(self.score_surface, (self.display_width - (self.display_width//3),
+                                                                             self.display_height//10))
+            w, h = self.score_surface.get_size()
+            self.score_x = (self.display_width - w)//2
+            self.score_y = int(self.die_y + self.die_size * 1.8)
+            self.display_surface.blit(self.score_surface, (self.score_x, self.score_y))
+
+    def display_points(self):
+        point_string = "Player's Points: " + str(self.game_rules.points)
+        points_surface = self.game_font.render(point_string, True, self.rust, None)
+        points_surface = pygame.transform.scale(points_surface, (self.display_width - (self.display_width//3),
+                                                                 self.display_height//10))
+        w, h = points_surface.get_size()
+        points_x = (self.display_width - w)//2
+        points_y = self.display_height - self.display_height//5
+        self.display_surface.blit(points_surface, (points_x, points_y))
+
+    def display_text(self):
+        """This functions displays all the text on the game."""
+        w, h = self.title_surface.get_size()
+        text_x = (self.display_width - w)//2
+        text_y = self.display_height//20
+        self.display_surface.blit(self.title_surface, (text_x, text_y))
+        if self.instruction_active:
+            temp_w, temp_h = self.instruction_surface.get_size()
+            temp_x = (self.display_width - temp_w)//2
+            temp_y = int(self.die_y - temp_h * 1.15)
+            self.display_surface.blit(self.instruction_surface, (temp_x, temp_y))
+
+    def display_all_die(self):
+        """Displays all die."""
+        for x in self.die_object_list:
+            x.display_die()
+
+    def display_all_buttons(self):
+        """Display all button in list that are active."""
+        for x in self.buttons_list:
+            if x.ACTIVE:
+                x.display_button()
+                x.display_highlighted()
+
+    def display_interface(self):
+        """Display all methods that are display related."""
+        self.display_surface.fill(self.background_color)
+        self.display_points()
+        self.display_score()
+        self.display_text()
+        self.display_all_die()
+        self.display_all_buttons()
+
     def fill_gradient(self, surface, color, gradient, rect=None, vertical=True, forward=True):
         """fill a surface with a gradient pattern
         Parameters:
@@ -101,9 +171,10 @@ class GraphicDieInterface:
         forward -> True=forward; False=reverse
 
         Pygame recipe: http://www.pygame.org/wiki/GradientCode"""
-        if rect is None: rect = surface.get_rect()
-        x1,x2 = rect.left, rect.right
-        y1,y2 = rect.top, rect.bottom
+        if rect is None:
+            rect = surface.get_rect()
+        x1, x2 = rect.left, rect.right
+        y1, y2 = rect.top, rect.bottom
         if vertical:
             h = y2-y1
         else:
@@ -113,42 +184,24 @@ class GraphicDieInterface:
         else:
             b, a = color, gradient
         rate = (
-            float(b[0]-a[0])/h,
-            float(b[1]-a[1])/h,
-            float(b[2]-a[2])/h
+            float(b[0] - a[0])/h,
+            float(b[1] - a[1])/h,
+            float(b[2] - a[2])/h
         )
         fn_line = pygame.draw.line
         if vertical:
             for line in range(y1,y2):
                 color = (
-                    min(max(a[0]+(rate[0]*(line-y1)),0),255),
-                    min(max(a[1]+(rate[1]*(line-y1)),0),255),
-                    min(max(a[2]+(rate[2]*(line-y1)),0),255)
+                    min(max(a[0] + (rate[0] * (line - y1)), 0), 255),
+                    min(max(a[1] + (rate[1] * (line - y1)), 0), 255),
+                    min(max(a[2] + (rate[2] * (line - y1)), 0), 255)
                 )
-                fn_line(surface, color, (x1,line), (x2,line))
+                fn_line(surface, color, (x1, line), (x2, line))
         else:
             for col in range(x1,x2):
                 color = (
-                    min(max(a[0]+(rate[0]*(col-x1)),0),255),
-                    min(max(a[1]+(rate[1]*(col-x1)),0),255),
-                    min(max(a[2]+(rate[2]*(col-x1)),0),255)
+                    min(max(a[0] + (rate[0] * (col - x1)), 0), 255),
+                    min(max(a[1] + (rate[1] * (col - x1)), 0), 255),
+                    min(max(a[2] + (rate[2] * (col - x1)), 0), 255)
                 )
-                fn_line(surface, color, (col,y1), (col,y2))
-
-    def display_dice(self):
-        """This function will display all dice."""
-        self.fill_gradient(self.display_surface, self.black, self.white)
-        self.die1.display_die()
-        self.die2.display_die()
-        self.die3.display_die()
-        self.die4.display_die()
-        self.die5.display_die()
-
-    def display_text(self):
-        """This functions displays all the text on the game."""
-        self.display_surface.blit(self.label, (self.half_x - (len( self.text)/2), self.half_y/2))
-
-    def display_interface(self):
-        """This functions displays everything that needs to go on the display surface."""
-        #self.display_text()
-        self.display_dice()
+                fn_line(surface, color, (col,y1), (col, y2))
